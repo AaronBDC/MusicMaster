96a9406565e282ad9815f3c5d30d31790623f87d
Aaron DC <aaronbdc@mail.com>
2017-06-01 08:04:23 -0400

    Initial commit. Spotify API key needed to work.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
 import React, {Component} from 'react';
+import './App.css';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+  search(){
+    console.log('this.state', this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search'
+
+    // old way
+    //const FETCH_URL = BASE_URL + 'q=' + this.state.query
+    //                + '&type=artist&limit=1';
+
+    // es6 way using template strings
+    const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+    
+
+    console.log('FETCH_URL', FETCH_URL);
+  }
   render(){
     return(
-      <div>
+      <div className="App">
         <div className="App-title">Music Master</div>
-        <div>
-          <input placeholder="search an artists..."/>
-          <button>button</button>
-        </div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl
+              type="text"
+              placeholder="Who's da Mastah?"
+              value={this.state.query}
+              onChange={event => {this.setState({query: event.target.value})}}
+              onKeyPress = {event => {
+                if (event.key === 'Enter') {
+                  this.search()
+                  }
+                }}
+              />
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="glyphicon glyphicon-sunglasses"></Glyphicon>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+
         <div className="Profile">
           <div>Artist Picture</div>
           <div>Artist Name</div>
